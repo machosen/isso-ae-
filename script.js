612404a850936fd0795cd69f7f2328c2ad6eecc5
machosen <161610465+machosen@users.noreply.github.com>
2024-06-12 11:50:17 -0300

    aula 12.06

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ const perguntas = [
         "Alternativa 1",
         "Alternativa 2"
     ]
-    
+},
+{
     enunciado: "Pergunta 2",
     alternativas: [
     "Alternativa 1",
     "Alternativa 2"
             ]
-        }
+        },
     ]; 
     
     
